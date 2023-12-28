61237ed7c7c0ea3bbdc06421a7d3db6ca84f3059
Sameer <ShatterdPotato>
2023-12-27 21:03:40 -0500

    woohoo

--- lua/ShatterdPotato/plugins/mason.lua
+++ lua/ShatterdPotato/plugins/mason.lua
@@ -1,10 +1,12 @@
 local mason = require("mason")
+local lsp_zero = require("lsp-zero")
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {
-  "pylsp"
+    "lua_ls"
+  },
+  handlers = {
+    lsp_zero.default_setup,
   },
 })
-
-
